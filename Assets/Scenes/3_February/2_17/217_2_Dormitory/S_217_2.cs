65b7fecfc732f41bd91732423544cc78fee84e96
chajinju <chajj0112@gmail.com>
2024-05-17 21:29:40 +0900

    unitysig

--- Assets/Scenes/3_February/2_17/217_2_Dormitory/S_217_2.cs
+++ Assets/Scenes/3_February/2_17/217_2_Dormitory/S_217_2.cs
@@ -2,75 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
-using System.IO;
 using UnityEngine.SceneManagement;
-using System;
 
-[System.Serializable]
-public class s_217_2
-{
-    public string name;
-    [TextArea]
-    public string dialogue;
-    public Sprite cg_M;
-    public Sprite cg_F;
-}
+
 public class S_217_2 : MonoBehaviour
 {
-    [SerializeField] private SpriteRenderer sprite_StandingCG;
-    [SerializeField] public TMP_Text txt_Name;
-    [SerializeField] public TMP_Text txt_Dialogue;
+    public Button Next_B;
 
-    private string savePath;
-    private int count = 0;
-    private bool isman;
-    private bool iswoman;
-    private string gabage;
-    private InfoMenu_S info;
-
-    [SerializeField] private s_217_2[] dialogue;
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        savePath = Application.persistentDataPath + "/save.txt";
-        StreamReader sr = new StreamReader(savePath);
-        gabage = sr.ReadLine();
-        gabage = sr.ReadLine();
-        isman = Convert.ToBoolean(sr.ReadLine());
-        iswoman = Convert.ToBoolean(sr.ReadLine());
-        count = 0;
-        NextDialogue();
         
     }
 
-    public void NextDialogue()
+    public void Next_B_P()
     {
-        if (count < dialogue.Length)
-        {
-            if (dialogue[count].name == "me")
-            {
-                StreamReader sr = new StreamReader(savePath);
-                txt_Name.text = sr.ReadLine();
-            }
-            else
-            {
-                txt_Name.text = dialogue[count].name;
-            }
-            txt_Dialogue.text = dialogue[count].dialogue;
-            if (isman)
-            {
-                sprite_StandingCG.sprite = dialogue[count].cg_M;
-            }
-            else if (iswoman)
-            {
-                sprite_StandingCG.sprite = dialogue[count].cg_F;
-            }
-            count++;
-        }
-        else
-        {
-            SceneManager.LoadScene(4);
-        }
-    }
+        SceneManager.LoadScene(4);
+    }    
 }
