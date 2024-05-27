2d8891e4604108e0ec871905d4e5ac93c7a29021
camaro13 <camaro13@naver.com>
2024-05-28 00:05:16 +0900

    unitysig

---
+++ Assets/Scenes/3_February/2_22/222_1/S_222_1_S_M.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_222_1_S_M : MonoBehaviour
+{
+    public MainMenu_s mainMenu_S;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (MainMenu_s.scenenumber == 15)
+        {
+            Destroy(this.gameObject);
+
+        }
+    }
+}
---
+++ Assets/Scenes/4_March/3_16/316_1/S_316_1_S_M.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_316_1_S_M : MonoBehaviour
+{
+    public MainMenu_s mainMenu_S;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (MainMenu_s.scenenumber == 26)
+        {
+            Destroy(this.gameObject);
+
+        }
+    }
+}
